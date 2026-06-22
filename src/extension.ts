93903536c8a2bee49bdee7b1d1392e265aaa8195
Br-Zueira <cauamesquitacevidanes12@gmail.com>
2026-06-21 21:25:49 -0300

    Added: Pickup Highlighted text

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const pickupText = vscode.commands.registerCommand('boilerplater.pickupText', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showWarningMessage('Open a file first!');
+    	    return;
+		}
+		const selection = editor.selection;
+		const highlightedCode = editor.document.getText(selection);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "boilerplater" is now active!');
+		if (!highlightedCode || highlightedCode.trim() === "") {
+        	vscode.window.showWarningMessage('Please highlight some code first!');
+        	return;
+    	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('boilerplater.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from boilerplater!');
+		vscode.window.showInformationMessage(`You highlighted: '${highlightedCode}'`)
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(pickupText);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
